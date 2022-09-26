493ccdbef2c30cb1a0445d1e560c1a04c66bbdf9
sheo-at-913292187539 <ir3LhesFc9L8UVxudTQwcnKAjMOAoDNyX2+yKq2iAws=>
2022-09-26 16:42:46 +0900

    add: add example code

---
+++ cloud-config-client-example/client/src/main/java/com/example/client/MyConfig.java
@@ -0,0 +1,19 @@
+package com.example.client;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@Setter
+@Getter
+@ConfigurationProperties("com.serin")
+@RefreshScope
+@ToString
+public class MyConfig {
+
+    private String profile;
+    private String region;
+
+}
